95066432728870d9cd652a136fda5ecfc478da88
Akash Reddy Jammula <akashreddyjammula@gmail.com>
2023-10-27 13:45:46 -0500

    Update app.js

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,13 +1,27 @@
-var MyApp = (function(){
+// var MyApp = (function(){
 
-    function init(uid, mid){
-        alert("From App js")
+//     function init(uid, mid){
+//         alert("From App js")
+//     }
+
+//     return {
+//         _init: function(uid, mid){
+//             init(uid, mid);
+//         }
+//     }
+// })
+
+var MyApp ={
+    _init: function (uid, mid) {
+        alert("From App js");
     }
+    // function init (uid, mid) {
+    //     alert("From App js");
+    // }
 
-
-    return {
-        _init: function(uid, mid){
-            Infinity(uid, mid);
-        }
-    }
-})
+    // return {
+    //     _init: function(uid, mid){
+    //         init(uid, mid);
+    //     },
+    // }
+};
