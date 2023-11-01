f23196ca3443adff265eea16cf97f35d2d02acb3
Akash Reddy Jammula <akashreddyjammula@gmail.com>
2023-11-01 12:31:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { Socket } = require("socket.io");
 var app = express();
 var server = app.listen(3000, function(){
     console.log("Listening on port 30000");
@@ -7,3 +8,7 @@ var server = app.listen(3000, function(){
 
 const io = require("socket.io")(server);
 app.use(express.static(path.join(__dirname, "")));
+
+io.on("connection", (Socket)=>{
+    console.log("socket id is ", socket.id);
+})
