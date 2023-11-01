53e2f8b45f6326154ea3534dfb9162fb8a1a9be7
Akash Reddy Jammula <akashreddyjammula@gmail.com>
2023-11-01 12:42:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var server = app.listen(3000, function(){
     console.log("Listening on port 30000");
 });
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+    allowEI03: true,
+});
 app.use(express.static(path.join(__dirname, "")));
 
 io.on("connection", (Socket)=>{
